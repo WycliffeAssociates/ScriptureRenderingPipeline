7ac4e3fb331ef70d6eab970e74521f95327c4da6
Craig Oliver <craig.robert.oliver@gmail.com>
2021-09-08 15:03:17 -0400

    Added more tests

--- SRPTests/RCLinkTests.cs
+++ SRPTests/RCLinkTests.cs
@@ -28,12 +28,33 @@ namespace SRPTests
         }
 
         [Test]
-        public void TestTMLink()
+        public void TestTmLink()
         {
             var ast = Markdown.Parse("[[rc://en/ta/man/translate/translate-dynamic]]", pipeline);
             var actual_html = Markdown.ToHtml(ast, pipeline);
             var expected_url = "/WycliffeAssociates/en_tm/src/branch/master/translate/translate-dynamic";
             var expected_html = $"<p><a href=\"{this.options.ServerUrl}{expected_url}\">{this.options.ServerUrl}{expected_url}</a></p>\n";
+            Assert.AreEqual(expected_html, actual_html);
+        }
+
+        [Test]
+        public void TestEmbeddedTmLink()
+        {
+            var ast = Markdown.Parse("Here is a link: [[rc://en/ta/man/translate/translate-dynamic]]", pipeline);
+            var actual_html = Markdown.ToHtml(ast, pipeline);
+            var expected_url = "/WycliffeAssociates/en_tm/src/branch/master/translate/translate-dynamic";
+            var expected_html = $"<p>Here is a link: <a href=\"{this.options.ServerUrl}{expected_url}\">{this.options.ServerUrl}{expected_url}</a></p>\n";
+            Assert.AreEqual(expected_html, actual_html);
+        }
+
+        [Test]
+        public void TestMultipleEmbeddedTmLinks()
+        {
+            var ast = Markdown.Parse("See also [[rc://en/ta/man/jit/figs-events]] and [[rc://en/ta/man/cobt/gl-strategy]]", pipeline);
+            var actual_html = Markdown.ToHtml(ast, pipeline);
+            var expected_url1 = "/WycliffeAssociates/en_tm/src/branch/master/jit/figs-events";
+            var expected_url2 = "/WycliffeAssociates/en_tm/src/branch/master/cobt/gl-strategy";
+            var expected_html = $"<p>See also <a href=\"{this.options.ServerUrl}{expected_url1}\">{this.options.ServerUrl}{expected_url1}</a> and <a href=\"{this.options.ServerUrl}{expected_url2}\">{this.options.ServerUrl}{expected_url2}</a></p>\n";
             Assert.AreEqual(expected_html, actual_html);
         }
     }
