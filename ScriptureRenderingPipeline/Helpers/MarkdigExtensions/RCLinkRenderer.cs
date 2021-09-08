cfe8f6c91841613ae64128b652118aa310ccc8db
Craig Oliver <craig.robert.oliver@gmail.com>
2021-09-08 16:12:54 -0400

    Got TA links working via regex

--- ScriptureRenderingPipeline/Helpers/MarkdigExtensions/RCLinkRenderer.cs
+++ ScriptureRenderingPipeline/Helpers/MarkdigExtensions/RCLinkRenderer.cs
@@ -4,46 +4,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ScriptureRenderingPipeline.Helpers.MarkdigExtensions
 {
     public class RCLinkRenderer : HtmlObjectRenderer<RCLink>
     {
+        private static Regex TA_LINK = new Regex(@"rc:\/\/([^\/]+)\/ta\/man\/([^]]+)", RegexOptions.Compiled);
+
+
+
         private readonly RCLinkOptions _options;
         public RCLinkRenderer(RCLinkOptions options)
         {
-            _options = options; 
+            _options = options;
         }
 
         protected override void Write(HtmlRenderer renderer, RCLink obj)
         {
-            if (renderer.EnableHtmlForInline)
+            if (!renderer.EnableHtmlForInline)
             {
-                string link = GenerateLink(obj);
+                return;
+            }
+
+            string linktext = obj.Link.ToString();
+            string url = "";
+            Match match;
+
+            match = TA_LINK.Match(linktext);
+            if (match.Success) {
+                var language = match.Groups[1];
+                var path = match.Groups[2];
+                var link = $"{_options.ServerUrl}/{_options.BaseUser}/{language}_tm/src/branch/master/{path}";
                 renderer.Write("<a href=\"").Write(link).Write("\">").Write(link).Write("</a>");
-            }
-        }
-
-        public string GenerateLink(RCLink input)
-        {
-            var rawLink = input.Link.ToString();
-            
-            // HACK: If the link doesn't contain slashes, for now ignore it.
-            if (!rawLink.Contains("/"))
-            {
-                return rawLink;
+                return;
             }
 
-            var splitLink = rawLink.Split("/");
-            var language = splitLink[2];
-            var resource = splitLink[3];
-            if (_options.ResourceOverrideMapping.ContainsKey(resource))
-            {
-                resource = _options.ResourceOverrideMapping[resource];
-            }
-            var path = string.Join("/", splitLink[5..]);
-            return $"{_options.ServerUrl}/{_options.BaseUser}/{language}_{resource}/src/branch/master/{path}";
+
+            // No match
+            renderer.Write(linktext);
         }
+
+        //public string GenerateLink(RCLink input)
+        //{
+        //    var rawLink = input.Link.ToString();
+
+        //    // HACK: If the link doesn't contain slashes, for now ignore it.
+        //    if (!rawLink.Contains("/"))
+        //    {
+        //        return rawLink;
+        //    }
+
+        //    var splitLink = rawLink.Split("/");
+        //    var language = splitLink[2];
+        //    var resource = splitLink[3];
+        //    if (_options.ResourceOverrideMapping.ContainsKey(resource))
+        //    {
+        //        resource = _options.ResourceOverrideMapping[resource];
+        //    }
+        //    var path = string.Join("/", splitLink[5..]);
+        //    return $"{_options.ServerUrl}/{_options.BaseUser}/{language}_{resource}/src/branch/master/{path}";
+        //}
     }
 }
