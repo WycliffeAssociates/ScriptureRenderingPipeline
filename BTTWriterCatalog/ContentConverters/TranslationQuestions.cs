20d0a6e38a549eaa212309588c6e3118ce69c3d1
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2021-09-15 16:04:43 -0400

    WIP

---
+++ BTTWriterCatalog/ContentConverters/TranslationQuestions.cs
@@ -0,0 +1,17 @@
+﻿using BTTWriterCatalog.Models;
+using PipelineCommon.Helpers;
+using PipelineCommon.Models.ResourceContainer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BTTWriterCatalog.ContentConverters
+{
+    public static class TranslationQuestions
+    {
+        public static void Convert(ZipFileSystem fileSystem, string basePath, string outputPath, ResourceContainer container, Dictionary<string,List<InputChunk>> chunks)
+        {
+
+        }
+    }
+}
