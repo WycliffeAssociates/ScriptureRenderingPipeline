836824ee39a6ef4b2b43d6b3692f270ed421099a
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2021-11-09 15:15:01 -0500

    Added missing util

---
+++ PipelineCommon/Helpers/CloudStorageUtils.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineCommon.Helpers
+{
+    public static class CloudStorageUtils
+    {
+        public static async Task<List<string>> ListAllFilesUnderPath(BlobContainerClient outputClient, string prefix)
+        {
+            var output = new List<string>();
+            var stack = new Stack<string>(new List<string>() { prefix});
+            while(stack.Count > 0)
+            {
+                var directory = stack.Pop();
+                await foreach (var file in outputClient.GetBlobsByHierarchyAsync(prefix: directory, delimiter: "/"))
+                {
+                    if (file.IsBlob)
+                    {
+                        output.Add(file.Blob.Name);
+                        continue;
+                    }
+                    // otherwise this is folder
+                    stack.Push(file.Prefix);
+
+                }
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// Upload files to Azure storage
+        /// </summary>
+        /// <param name="log"></param>
+        /// <param name="connectionString"></param>
+        /// <param name="outputContainer"></param>
+        /// <param name="sourceDir"></param>
+        /// <param name="basePath"></param>
+        /// <returns></returns>
+        public static async Task UploadToStorage(ILogger log, string connectionString, string outputContainer, string sourceDir, string basePath)
+        {
+            var extentionToMimeTypeMatching = new Dictionary<string, string>()
+            {
+                [".html"] = "text/html",
+                [".json"] = "application/json",
+            };
+            BlobContainerClient outputClient = new BlobContainerClient(connectionString, outputContainer);
+            outputClient.CreateIfNotExists();
+            List<Task> uploadTasks = new List<Task>();
+            foreach(var file in Directory.GetFiles(sourceDir, "*.*", SearchOption.AllDirectories))
+            {
+                var relativePath = Path.GetRelativePath(sourceDir, file);
+                var extension = Path.GetExtension(relativePath);
+                log.LogDebug($"Uploading {relativePath}");
+                var tmp = outputClient.GetBlobClient(Path.Join(basePath,relativePath ).Replace("\\","/"));
+                tmp.DeleteIfExists();
+                string contentType = extentionToMimeTypeMatching.ContainsKey(extension) ? extentionToMimeTypeMatching[extension] : "application/octet-stream";
+                uploadTasks.Add(tmp.UploadAsync(file, new BlobUploadOptions() { HttpHeaders = new BlobHttpHeaders() { ContentType = contentType } }));
+            };
+            await Task.WhenAll(uploadTasks);
+        }
+    }
+}
