d74f0e5415bf995d2e8f6a477218f7f4e7d617ac
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2023-10-16 14:30:28 -0400

    More tests and changes from testing

--- PipelineCommon/Models/BusMessages/RenderingResultMessage.cs
+++ PipelineCommon/Models/BusMessages/RenderingResultMessage.cs
@@ -6,6 +6,7 @@ public class RenderingResultMessage
 	public string Message { get; set; }
 	public string User { get; set; }
 	public string Repo { get; set; }
+	public int RepoId { get; set; }
 
 	public RenderingResultMessage()
 	{
@@ -16,5 +17,6 @@ public class RenderingResultMessage
 	{
 		User = source.User;
 		Repo = source.Repo;
+		RepoId = source.RepoId;
 	}
 }
---
+++ SRPTests/ModelTests.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using PipelineCommon.Models.BusMessages;
+
+namespace SRPTests;
+
+public class ModelTests
+{
+    private const string user = "user";
+    private const string repo = "repo";
+    private const int repoId = 233;
+    
+    [Test]
+    public void TestVerseCountResultFromWACSMessage()
+    {
+        var message = new WACSMessage()
+        {
+            User = user,
+            Repo = repo,
+            RepoId = repoId,
+        };
+        var verseCountResult = new VerseCountingResult(message);
+        Assert.AreEqual(user, verseCountResult.User);
+        Assert.AreEqual(repo, verseCountResult.Repo);
+        Assert.AreEqual(repoId, verseCountResult.RepoId);
+    }
+
+    [Test]
+    public void TestRenderingResultFromWACSMessage()
+    {
+        var message = new WACSMessage()
+        {
+            User = user,
+            Repo = repo,
+            RepoId = repoId,
+        };
+        var renderingResult = new RenderingResultMessage(message);
+        Assert.AreEqual(user, renderingResult.User);
+        Assert.AreEqual(repo, renderingResult.Repo);
+        Assert.AreEqual(repoId, renderingResult.RepoId);
+    }
+}
