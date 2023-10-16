3e31268ff28823bb9f30c309121c9c6aa0e86f4d
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2023-10-16 10:08:22 -0400

    More tests and improvements based on testing

--- SRPTests/ZipFileSystemTests.cs
+++ SRPTests/ZipFileSystemTests.cs
@@ -6,13 +6,22 @@ using PipelineCommon.Helpers;
 
 namespace SRPTests;
 
+/// <summary>
+/// Test for the ZipFileSystem
+/// </summary>
 public class ZipFileSystemTests
 {
     private MemoryStream stream;
     private static string testFileName = "test.txt";
     private static string testFileContents = "test";
     private static string nestedFolder = "folder";
-    private static string nestedFileName = $"{nestedFolder}nested.txt";
+    private static string furtherNestedFolder = $"{nestedFolder}/folder";
+    private static string nestedFileName = $"{nestedFolder}/nested.txt";
+    private static string otherNestedFileName = $"{nestedFolder}/nested.oth";
+    
+    /// <summary>
+    /// Initialize things for tests
+    /// </summary>
     [SetUp]
     public void SetUp()
     {
@@ -20,11 +29,19 @@ public class ZipFileSystemTests
         using var archive = new ZipArchive(stream, ZipArchiveMode.Create, true);
         AddFileToZip(archive, testFileName, testFileContents);
         AddFileToZip(archive, "test.log", "log contents");
-        AddFileToZip(archive, nestedFileName, "nested file name");
+        AddFileToZip(archive, nestedFileName, "nested file");
+        AddFileToZip(archive, otherNestedFileName, "other nested file");
         AddFolderToZip(archive, nestedFolder);
+        AddFolderToZip(archive, furtherNestedFolder);
         stream.Position = 0;
     }
 
+    /// <summary>
+    /// Add a file to the zip
+    /// </summary>
+    /// <param name="archive">The archive to add it to</param>
+    /// <param name="filePath">The path of the file to add</param>
+    /// <param name="fileContents">The contents of the file</param>
     private void AddFileToZip(ZipArchive archive, string filePath, string fileContents)
     {
         var tmpPath = Path.Combine(Path.GetTempPath(), "tmp.txt");
@@ -37,6 +54,11 @@ public class ZipFileSystemTests
         File.Delete(tmpPath);
     }
 
+    /// <summary>
+    /// Add a folder to the zip
+    /// </summary>
+    /// <param name="archive">The archive to add it to</param>
+    /// <param name="folderPath">The folder path to add</param>
     private void AddFolderToZip(ZipArchive archive, string folderPath)
     {
         var entry = archive.CreateEntry(folderPath);
@@ -44,18 +66,24 @@ public class ZipFileSystemTests
         folderStream.Close();
     }
 
+    /// <summary>
+    /// Verify that GetAllFiles returns the correct files with and without a pattern
+    /// </summary>
     [Test]
     public void TestGetAllFiles()
     {
         var fileSystem = new ZipFileSystem(stream);
         var allFiles = fileSystem.GetAllFiles().ToList();
         var allLogs = fileSystem.GetAllFiles(".log").ToList();
-        Assert.AreEqual(3, allFiles.Count());
+        Assert.AreEqual(4, allFiles.Count());
         Assert.AreEqual("test.txt", allFiles[0]);
         Assert.AreEqual(1, allLogs.Count);
         fileSystem.Close();
     }
 
+    /// <summary>
+    /// Verify that FileExists works correctly
+    /// </summary>
     [Test]
     public void TestFileExists()
     {
@@ -64,12 +92,40 @@ public class ZipFileSystemTests
         Assert.IsFalse(fileSystem.FileExists("RandomName.txt"));
     }
 
+    /// <summary>
+    /// Verify that GetFolders returns the correct number of folders
+    /// </summary>
     [Test]
     public void TestGetFolders()
     {
         var fileSystem = new ZipFileSystem(stream);
         var folders = fileSystem.GetFolders().ToList();
         Assert.AreEqual(1, folders.Count);
+        folders = fileSystem.GetFolders(nestedFolder + "/").ToList();
+        Assert.AreEqual(1, folders.Count);
+        folders = fileSystem.GetFolders(nestedFolder).ToList();
+        Assert.AreEqual(1, folders.Count);
+    }
+
+    /// <summary>
+    /// Test that joining paths works correctly
+    /// </summary>
+    [Test]
+    public void TestJoin()
+    {
+        var fileSystem = new ZipFileSystem(stream);
+        Assert.AreEqual("first/second.txt",fileSystem.Join("first", "second.txt"));
+    }
+
+    /// <summary>
+    /// Verify that GetFiles returns the correct files with and without a pattern
+    /// </summary>
+    [Test]
+    public void TestGetFiles()
+    {
+        var fileSystem = new ZipFileSystem(stream);
+        Assert.AreEqual(2, fileSystem.GetFiles(nestedFolder).Count());
+        Assert.AreEqual(1, fileSystem.GetFiles(nestedFolder, ".txt").Count());
     }
 
     /*
