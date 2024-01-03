61a48674b03950dd232b821b4db44bdb3f765635
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2024-01-03 14:21:17 -0500

    Added missing application insights in the worker

--- ScriptureRenderingPipelineWorker/Program.cs
+++ ScriptureRenderingPipelineWorker/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices((context, services) =>
     {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
         services.AddAzureClients(clientBuilder =>
         {
             clientBuilder.AddServiceBusClient(context.Configuration.GetValue<string>("ServiceBusConnectionString")).WithName("ServiceBusClient");
