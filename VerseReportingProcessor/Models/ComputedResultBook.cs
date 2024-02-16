000ae61d04ec1b7c5bb463b8464b8d45db1bac67
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2024-02-16 10:38:24 -0500

    If a book is empty then don't include that in the PORT count

--- VerseReportingProcessor/Models/ComputedResultBook.cs
+++ VerseReportingProcessor/Models/ComputedResultBook.cs
@@ -5,5 +5,6 @@ public class ComputedResultBook
     public string Slug { get; set; }
     public int ExpectedChapters { get; set; }
     public int ActualChapters { get; set; }
+    public bool IsEmpty { get; set; }
     public List<ComputedResultChapter> Chapters { get; set; } = new();
 }
