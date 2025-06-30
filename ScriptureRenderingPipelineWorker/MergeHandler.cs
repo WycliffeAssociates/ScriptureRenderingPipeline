1eb8da239cfd6fcbd1645fa929fe95fc839f7d4d
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2025-06-30 11:40:15 -0400

    WIP

--- ScriptureRenderingPipelineWorker/MergeHandler.cs
+++ ScriptureRenderingPipelineWorker/MergeHandler.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Security.Cryptography;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Azure;
@@ -10,10 +11,15 @@ using Microsoft.Extensions.Configuration;
 using PipelineCommon.Helpers;
 using PipelineCommon.Models.BusMessages;
 using PipelineCommon.Models.ResourceContainer;
+using ScriptureBurrito;
+using ScriptureBurrito.Models;
 using USFMToolsSharp;
 using USFMToolsSharp.Models.Markers;
 using USFMToolsSharp.Renderers.USFM;
 using YamlDotNet.Serialization;
+using Generator = BTTWriterLib.Models.Generator;
+using Language = PipelineCommon.Models.ResourceContainer.Language;
+using License = System.ComponentModel.License;
 
 namespace ScriptureRenderingPipelineWorker;
 
@@ -62,6 +68,7 @@ public class MergeTrigger
         var projects = new List<Project>();
         var contributors = new List<string>();
         var sources = new List<Source>();
+        var contentForBurrito = new List<ContentForBurrito>();
         foreach (var repo in message.ReposToMerge)
         {
 	        var info = await Utils.GetGiteaRepoInformation(repo.HtmlUrl, repo.User, repo.Repo);
@@ -81,7 +88,7 @@ public class MergeTrigger
 	        if (repoInformation.isBTTWriterProject)
 	        {
 		        _log.LogDebug("Merging BTT Writer project");
-		        MergeWriterProject(projectZip, basePath, repo, output, renderer);
+		        MergeWriterProject(projectZip, basePath, repo, output, renderer, contentForBurrito);
 		        var tmpProject = repoInformation.ResourceContainer.projects[0];
 		        tmpProject.path = $"./{tmpProject.identifier}.usfm";
 		        projects.Add(tmpProject);
@@ -89,7 +96,7 @@ public class MergeTrigger
 	        else
 	        {
 		        _log.LogDebug("Merging USFM project");
-		        await MergeUSFMProject(projectZip, output);
+		        await MergeUSFMProject(projectZip, output, contentForBurrito);
 				projects.AddRange(repoInformation.ResourceContainer.projects);
 	        }
 			contributors.AddRange(repoInformation?.ResourceContainer?.dublin_core?.contributor ?? []);
@@ -137,6 +144,10 @@ public class MergeTrigger
         var repoName = $"merged-{languageCode}";
         _log.LogInformation("Uploading into {User}/{Repo}", _destinationUser, repoName);
         
+        // Create a scripture burrito for thie merged repo
+        var burrito = CreateBurrito("Bible", "bible", languageCode, languageName, languageDirection, contentForBurrito);
+        
+        output.Add("metadata.json", BurritoSerializer.Serialize(burrito));
         var existingRepo = await _giteaClient.GetRepository(_destinationUser, repoName);
         if (existingRepo != null)
 		{
@@ -146,29 +157,51 @@ public class MergeTrigger
 			return new MergeResult(true, $"{_giteaBaseAddress}/{_destinationUser}/{repoName}/src/branch/{newBranchName}", message.RequestingUserName,
 				languageCode, _destinationUser, repoName, existingRepo.Id, mergedPORTRepoIds);
 		}
+        
 
         var createdRepoId = await UploadContentToNewRepo(_destinationUser, repoName, output);
         return new MergeResult(true, $"{_giteaBaseAddress}/{_destinationUser}/{repoName}", message.RequestingUserName,
 	        languageCode, _destinationUser, repoName, createdRepoId, mergedPORTRepoIds);
     }
 
-    private static async Task MergeUSFMProject(ZipFileSystem projectZip, Dictionary<string, string> output)
+    private static async Task MergeUSFMProject(ZipFileSystem projectZip, Dictionary<string, string> output, List<ContentForBurrito> outputForBurrito)
     {
+	    var parser = new USFMParser(ignoreUnknownMarkers: true);
 	    foreach (var file in projectZip.GetAllFiles(".usfm"))
 	    {
 		    var content = await projectZip.ReadAllTextAsync(file);
+		    var doc = parser.ParseFromString(content);
+		    var toc1 = doc.GetChildMarkers<TOC1Marker>().FirstOrDefault();
+		    var toc2 = doc.GetChildMarkers<TOC2Marker>().FirstOrDefault();
+		    var toc3 = doc.GetChildMarkers<TOC3Marker>().FirstOrDefault();
 		    var fileName = Path.GetFileName(file);
+		    outputForBurrito.Add(new()
+		    {
+			    BookCode = toc3.BookAbbreviation.ToUpper(),
+			    BookLongName = toc1.LongTableOfContentsText,
+			    BookName = toc2.ShortTableOfContentsText,
+			    Path = fileName,
+			    Size = (uint)content.Length,
+                MD5Hash = HashString(content),
+		    });
 		    output.TryAdd(fileName, content);
 	    }
     }
 
-    private void MergeWriterProject(ZipFileSystem projectZip, string? basePath, MergeRequestRepo repo, Dictionary<string, string> output,
-	    USFMRenderer renderer)
+    private static string HashString(string input)
+    {
+	    return MD5.HashData(System.Text.Encoding.Unicode.GetBytes(input))
+		    .Aggregate("", (s, b) => s + b.ToString("x2"));
+    }
+
+    private void MergeWriterProject(ZipFileSystem projectZip, string? basePath, MergeRequestRepo repo,
+	    Dictionary<string, string> output,
+	    USFMRenderer renderer, List<ContentForBurrito> contentForBurrito)
     {
 	    var container = new ZipFileSystemBTTWriterLoader(projectZip,basePath);
 	    var usfmObject = BTTWriterLoader.CreateUSFMDocumentFromContainer(container, false, new USFMParser(ignoreUnknownMarkers: true));
 	    var bookCode = usfmObject.GetChildMarkers<TOC3Marker>().FirstOrDefault()?.BookAbbreviation;
-				
+	    
 	    if (bookCode == null)
 	    {
 		    _log.LogWarning("No book code found for {User}/{Repo}", repo.User, repo.Repo);
@@ -181,9 +214,18 @@ public class MergeTrigger
 	    {
 		    return;
 	    }
-	    
 				
 	    var usfm = renderer.Render(usfmObject);
+	    contentForBurrito.Add(new()
+	    {
+		    BookCode = bookCode.ToUpper(),
+		    BookName = usfmObject.GetChildMarkers<TOC2Marker>().FirstOrDefault()?.ShortTableOfContentsText ?? bookCode,
+		    BookLongName = usfmObject.GetChildMarkers<TOC1Marker>().FirstOrDefault()?.LongTableOfContentsText ?? bookCode,
+		    MD5Hash = HashString(usfm),
+		    Size = (uint)usfm.Length,
+		    Path = fileName,
+	    });
+	    
 	    output.Add(fileName, usfm);
     }
 
@@ -228,4 +270,133 @@ public class MergeTrigger
 	    var zipStream = await result.Content.ReadAsStreamAsync();
 	    return new ZipFileSystem(zipStream);
     }
+    private static SerializationRoot CreateBurrito(string projectName, string projectAbbreviation, string languageCode, string languageName, string languageTextDirection, List<ContentForBurrito> content)
+    {
+        return new SerializationRoot()
+        {
+            Meta = new Meta()
+            {
+                Version = "1.0.0",
+                Category = "source",
+                Generator = new ScriptureBurrito.Models.Generator() 
+                {
+                    SoftwareName = "Repo consolidator",
+                    SoftwareVersion = "1.0.0",
+                    UserName = "rbnswartz"
+                },
+                DefaultLocale = "en",
+                DateCreated = DateTime.Now,
+                Normalization = "NFC"
+            },
+            IdAuthorities = new()
+            {
+                ["wycliffeassociates"] = new Authority()
+                {
+                    Id = "https://www.wycliffeassociates.org",
+                    Name = new Dictionary<string, string>()
+                    {
+                        ["en"] = "Wycliffe Associates"
+                    }
+                }
+            },
+            Identification = new Identification()
+            {
+                Primary = new()
+                {
+                    ["wycliffeassociatse"] = new()
+                    {
+                        [Guid.NewGuid().ToString()] = new Revision()
+                        {
+                            RevisionId = "1.0.0",
+                            TimeStamp = DateTime.Now
+                        }
+                    }
+                },
+                Name = new()
+                {
+                    ["en"] = projectName,
+                },
+                Abbreviation = new()
+                {
+                    ["en"] = projectAbbreviation,
+                }
+            },
+            Confidential = false,
+            Languages = [
+                new ScriptureBurrito.Models.Language()
+                {
+                    Tag = languageCode,
+                    Name = new Dictionary<string, string>()
+                    {
+                        ["en"] = languageName,
+                    },
+                    ScriptDirection = languageTextDirection
+                }
+            ],
+            Type = new()
+            {
+                FlavorType = new()
+                {
+                    Name = "scripture",
+                    Flavor = new Flavor()
+                    {
+                        Name = "textTranslation",
+                        ProjectType = "standard",
+                        TranslationType = "firstTranslation",
+                        Audience = "common",
+                        UsfmVersion = "3.0"
+                    },
+                    CurrentScope = content.ToDictionary(i => i.BookCode, i => new List<string>())
+                }
+            },
+            Copyright = new()
+            {
+                Licenses = new List<ScriptureBurrito.Models.License>()
+                {
+                    new ScriptureBurrito.Models.License()
+                    {
+                        Ingredient = "LICENSE.md",
+                    }
+                }
+            }, //Let's see if this works without it
+            LocalizedNames = content.ToDictionary(i => i.BookCode, i => new LocalizedName()
+            {
+                // TODO: Bring in other things instead of just the book code
+                Short = new ()
+                {
+                    ["en"] = i.BookCode
+                },
+                Abbreviation = new ()
+                {
+                    ["en"] = i.BookCode
+                },
+                Long = new ()
+                {
+                    ["en"] = i.BookCode
+                }
+            }),
+            Ingredients = content.ToDictionary(i => i.Path, i => new Ingredient()
+            {
+                Checksum = new Checksum()
+                {
+                    MD5 = i.MD5Hash
+                },
+                MimeType = "text/x-usfm",
+                Size = i.Size,
+                Scope = new ()
+                {
+                    [i.BookCode] = new()
+                }
+            })
+        };
+    }
+}
+public class ContentForBurrito
+{
+    public string Path { get; set; }
+    public string BookCode { get; set; }
+    public string MD5Hash { get; set; }
+    public uint Size { get; set; }
+    public string BookName { get; set; }
+    public string BookLongName { get; set; }
 }
